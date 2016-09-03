45fafe8d7135847c6563a3de2aa3b0c2c78bb0a1
Dmitry Kozliuk <dmitry.kozliuk@gmail.com>
2016-09-03 21:30:58 +0300

    ТП: избавление от make.bat и нормальная работа CMake
    Вместо `make.bat` прописан alias в lab.bat, заодно не нужно копировать
    `make.bat` по всем каталогам.
    
    CMake требует, чтобы в `%PATH%` не было `sh.exe`, а он есть у Git.
    Но т. к. Git запускать из командной строки нам не нужно (используем Git
    Bash), вообще не добавляем его в `%PATH%`.

--- study/courses/sdt/lab.bat
+++ study/courses/sdt/lab.bat
@@ -1,10 +1,11 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal EnableDelayedExpansion
 
-set PATH=!PATH!;C:\Program Files\Git\bin
 set PATH=!PATH!;C:\Program Files\Cppcheck
 set PATH=!PATH!;C:\Program Files\doxygen\bin
 set PATH=!PATH!;C:\Program Files\CMake\bin
 set PATH=!PATH!;C:\mingw-w64\mingw32\bin
+
+doskey make=mingw32-make.exe $*
 
 cmd.exe /k
--- study/courses/sdt/make.bat
+++
@@ -1,1 +0,0 @@
-@C:\MinGW-w64\mingw32\bin\mingw32-make.exe %*
