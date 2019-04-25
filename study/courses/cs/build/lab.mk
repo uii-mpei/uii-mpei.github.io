b049bdac001d2ac74268b1d7c136a78e1fd3e015
Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
2019-04-26 01:56:41 +0300

    РПОСУ: ЛР 4: задание в Markdown, указания из 2018

--- study/courses/cs/build/lab.mk
+++ study/courses/cs/build/lab.mk
@@ -2,25 +2,29 @@
 ifndef ROOT
 	$(error ROOT variable not set to course root)
 endif
-ifndef PAGE
-	$(error PAGE variable not set to lab markdown file name)
+ifndef SOURCE
+	$(error SOURCE variable not set to lab markdown file name)
+endif
+ifndef TARGET
+	$(error TARGET variable not set to lab HTML file name)
 endif
 
 include $(ROOT)/build/rm.mk
 
 .PHONY: all clean
 
-all: index.html
+all: $(TARGET)
 
-index.html: $(PAGE)
+$(TARGET): $(SOURCE)
 	pandoc \
 		--include-before-body "$(ROOT)/header.html" \
 		--include-after-body "$(ROOT)/footer.html" \
 		--toc \
 		--standalone \
+		--mathjax \
 		--from markdown+smart \
 		--to html \
 		"$^" >"$@"
 
 clean:
-	$(RM) index.html
+	$(RM) $(TARGET)
